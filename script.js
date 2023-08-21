62dc9be71448f4ca19abc1eb5a08ceaabe74b920
Jonathan Thösing-Jörgensen <jonathan.thosingjor@learnet.se>
2023-08-21 13:33:54 +0200

    Updated portfolio

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+let darkMode = new Boolean(true);
+
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
+    }
+  }
+
+  function colorChange() {
+    if (darkMode == true) {
+        console.log("light");
+        const root_theme = document.querySelector(':root');
+        root_theme.style.setProperty('--blurBackground', 'rgba( 255, 255, 255, 0.3 )');
+        root_theme.style.setProperty('--blurText', 'rgb( 0, 0, 0 )');
+        root_theme.style.setProperty('--navHover', 'rgba( 100, 100, 100, 0.3)');
+        document.body.style.backgroundImage = "url('assets/img/lightBg.jpg')";
+        document.getElementById("darkToggle").innerText = "Darkmode";
+        darkMode = false;
+    }
+    else {
+        console.log("dark");
+        const root_theme = document.querySelector(':root');
+        root_theme.style.setProperty('--blurBackground', 'rgba( 10, 10, 10, 0.7 )');
+        root_theme.style.setProperty('--blurText', 'rgb( 220, 220, 220 )');
+        root_theme.style.setProperty('--navHover', 'rgb( 200, 200, 200)');
+        document.body.style.backgroundImage = "url('assets/img/darkBg.jpg')";
+        document.getElementById("darkToggle").innerText = "Lightmode";
+        darkMode = true;
     }
   }
